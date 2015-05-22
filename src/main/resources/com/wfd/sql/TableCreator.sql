96ec43c8d99c1c0bd51ed0a759b5d5ffea26cd58
YuweiZeng <gzlzhz@126.com>
2015-05-22 11:11:57 +0800

    convert derby to mysql creator

--- src/main/resources/com/wfd/sql/TableCreator.sql
+++ src/main/resources/com/wfd/sql/TableCreator.sql
@@ -1,5 +1,5 @@
 CREATE TABLE t_users(
-user_id INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),
+user_id INTEGER NOT NULL AUTO_INCREMENT,
 username VARCHAR(255) NOT NULL,
 password VARCHAR(255) NOT NULL,
 securityquestion VARCHAR(255) DEFAULT '',
@@ -7,7 +7,7 @@ securityanswer VARCHAR(255) DEFAULT '',
 CONSTRAINT pk_user_id PRIMARY KEY (user_id)
 );
 CREATE TABLE t_post(
-post_id INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),
+post_id INTEGER NOT NULL AUTO_INCREMENT,
 user_id INTEGER,
 content VARCHAR(2000),
 deleted BOOLEAN DEFAULT FALSE,
@@ -18,7 +18,7 @@ CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES t_users(user_id)
 
 /*Create topic infomation table*/
 CREATE TABLE t_topic(
-topic_id INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),
+topic_id INTEGER NOT NULL AUTO_INCREMENT,
 post_id INTEGER,
 support_count INTEGER,
 disagree_count INTEGER,
@@ -28,7 +28,7 @@ CONSTRAINT fk_topic_post FOREIGN KEY (post_id) REFERENCES t_post(post_id)
 
 /*Create comment information table*/
 CREATE TABLE t_comment(
-comment_id INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),
+comment_id INTEGER NOT NULL AUTO_INCREMENT,
 post_id INTEGER,
 CONSTRAINT pk_comment_id PRIMARY KEY (comment_id),
 CONSTRAINT fk_comment_post FOREIGN KEY (post_id) REFERENCES t_post(post_id)
@@ -36,8 +36,8 @@ CONSTRAINT fk_comment_post FOREIGN KEY (post_id) REFERENCES t_post(post_id)
 
 /*Create comment information table*/
 CREATE TABLE t_post_relation(
-parent_id INTEGER,
-child_id INTEGER,
+parent_id INTEGER NOT NULL,
+child_id INTEGER NOT NULL,
 CONSTRAINT pk_relation_id PRIMARY KEY (parent_id,child_id)
 );
 
